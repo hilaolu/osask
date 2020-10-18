216f3585524f40cc2b88267e74a337a236e58b1d
hilaolu <i@phyzait.moe>
2020-10-19 01:02:13 +0800

    try to scan disk

--- bootloader.s
+++ bootloader.s
@@ -24,32 +24,62 @@ start:
 
 entry:
 	mov $0,%ax /*compared to intel syntax,operand and operator is reversed*/
-    mov %ax,%ds
-    mov %ax,%es /*direct operator -> $*/
-    mov %ax,%ss /*register -> %*/
+    mov %ax,%ss
     mov $0x7c00,%sp
+    mov %ax,%ds
 
-    mov $msg,%si /*label use $*/
+    mov $0x0820,%ax
+    mov %ax,%es
+    mov $0,%ch
+    mov $0,%dh
+    mov $2,%cl
+
+    mov $0,%si
+
+retry:
+    mov $0x02,%ah
+    mov $1,%al
+    mov $0,%bx
+    mov $0x00,%dl
+    int $0x13
+    jnc ok
+    add $1,%si
+    cmp $5,%si
+    jae error
+    mov $0x00,%ah
+    mov $0x00,%dl
+    int $0x13
+    jmp retry
+
+ok:
+    mov $okmsg,%si
+    call putloop
+    jc fin
+
+fin:
+    hlt
+    jmp fin
+
+error:
+    mov $msg,%si
     call putloop
 
 
 putloop:
-    movb (%si),%al
+    movb (%si),%al /*cpu doesn't knows how long the first operand is*/
     add $1,%si
-    cmp $0,%al
-    je fin
+    cmp $0,%al /*compare*/
+    je fin /*jump if equal*/
     movb $0x0e,%ah
     movw $15,%bx
-    int $0x10
+    int $0x10 /*interrupt*/
     jmp putloop
-fin:
-    hlt
-    ret
+
 
 msg:
-    .asciz "\r\nbootloader is running"
-    .asciz "\r\nhihi"
-
+    .asciz "\r\nload failed"
+okmsg:
+    .asciz "\r\nok"
 
 .org 510
 .word 0xaa55
